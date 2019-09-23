7d6b8f0d059002a900028da758fc5088b2a79264
LiuZhaohui666 <39998225+LiuZhaohui666@users.noreply.github.com>
2019-09-23 14:54:05 +0800

    Update equalization.cpp

--- src/equalization.cpp
+++ src/equalization.cpp
@@ -13,8 +13,8 @@
 
 #define LINEAR_X 0
 
-using namespace cv;
 
+using namespace cv;
 
 class Histogram1D
 {
@@ -77,115 +77,137 @@ public:
     }
 };
 
-
 int main(int argc, char **argv)
 {
-    VideoCapture capture;
-    capture.open(1);//打开zed相机
+	VideoCapture capture;
+        capture.open(0);//打开zed相机
 
 
-    ROS_WARN("*****START");
-    ros::init(argc,argv,"trafficLaneTrack");//初始化ROS节点
-    ros::NodeHandle n;
+	ROS_WARN("*****START");
+	ros::init(argc,argv,"trafficLaneTrack");//初始化ROS节点
+        ros::NodeHandle n;
 
-    // ros::Rate loop_rate(10);//定义速度发布频率
-    ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/smoother_cmd_vel", 5);//定义速度发布器
-
-
-    if (!capture.isOpened())
-    {
-        printf("摄像头没有正常打开，重新插拔工控机上当摄像头\n");
-        return 0;
-    }
-    waitKey(1000);
-    Mat img;//当前帧图片
-    int nFrames = 0;//图片帧数
-    int frameWidth = capture.get(CV_CAP_PROP_FRAME_WIDTH);//图片宽
-    int frameHeight = capture.get(CV_CAP_PROP_FRAME_HEIGHT);//图片高
+        // ros::Rate loop_rate(10);//定义速度发布频率
+        ros::Publisher pub = n.advertise<geometry_msgs::Twist>("/smoother_cmd_vel", 5);//定义速度发布器
 
 
-    while (ros::ok())
-    {
-        capture.read(img);
-        if(img.empty())
+	if (!capture.isOpened())
+	{
+		printf("摄像头没有正常打开，重新插拔工控机上当摄像头\n");
+		return 0;
+	}
+	waitKey(1000);
+	Mat img;//当前帧图片  
+	int nFrames = 0;//图片帧数
+	int frameWidth = capture.get(CV_CAP_PROP_FRAME_WIDTH);//图片宽
+	int frameHeight = capture.get(CV_CAP_PROP_FRAME_HEIGHT);//图片高
+
+
+	while (ros::ok())
+	{
+		capture.read(img);
+		if(img.empty())
+		{
+			break;
+		}
+                Mat frame;
+                cvtColor(img,frame, CV_BGR2GRAY );  //彩色图片转换成黑白图片
+
+		Mat frIn = frame(cv::Rect(0, 0, frame.cols, frame.rows));//截取zed的左目图片
+
+		// 此处增加直方图均衡化
+
+        int nRows = frIn.rows;
+        int nCols = frIn.cols;
+        int nSumPix[256];               //统计每个灰度下的像素个数
+        double nProDis[256];            //统计灰度频率
+        double nSumProDis[256];         //计算累计密度
+        int EqualizeSumPix[256];        //重新计算均衡化后的灰度值，四舍五入
+
+
+        //初始化
+        for (int i = 0; i < 256; i++)
         {
-            break;
+            nSumPix[i] = 0;
+            nProDis[i] = 0.0;
+            nSumProDis[i] = 0.0;
+            EqualizeSumPix[i] = 0;
         }
-        Mat frame;
-        cvtColor(img,frame, CV_BGR2GRAY );  //彩色图片转换成黑白图片
-        Mat frIn = frame(cv::Rect(0, 0, frame.cols / 2, frame.rows));//截取zed的左目图片
 
-        // 此处增加直方图均衡化
-        Mat frOut = frIn.clone();//定义输出图像
-        int gray[256] = {0};
-        double gray_prob[256] = {0};//图像的灰度分布概率
-        double gray_integral[256] = {0};  //图像的灰度累计密度
-        int gray_equal[256] = {0};  //均衡化后的灰度值
-        int pixel_sum = 0;//像素总量
-        pixel_sum = frIn.cols*frIn.rows;
-        for (int i = 0; i < frIn.rows; i++)
+        //统计每个灰度下的像素个数
+        for (int i = 0; i < nRows; i++)
         {
-            uchar* p = frIn.ptr<uchar>(i);
-            for(int j = 0; j < frIn.cols; j++)
+            for (int j = 0; j < nCols; j++)
             {
-                int value = p[j];
-                gray[value]++;
+                nSumPix[(int)frIn.at<uchar>(i, j)]++;
             }
-        }//计算每个灰度像素个数
-        for (int i = 0; i < 256 ; i++)
+        }
+
+        //统计灰度频率
+        for (int i = 0; i < 256; i++)
         {
-            gray_prob[i] = ((double)gray[i]/pixel_sum);
-        }//计算分布概率
-        gray_integral[0] = gray_prob[0];
+            nProDis[i] = (double)nSumPix[i] / (nRows * nCols);
+        }
+
+        //计算累计密度
+        nSumProDis[0] = nProDis[0];
         for (int i = 1; i < 256; i++)
         {
-            gray_integral[i] = gray_integral[i-1] + gray_prob[i];
+            nSumProDis[i] = nSumProDis[i - 1] + nProDis[i];
         }
-        for (int i = 0; i < 256 ; i++)
+
+        //重新计算均衡化后的灰度值，四舍五入
+        for (int i = 0; i < 256; i++)
         {
-            gray_equal[i] = (uchar)(255 * gray_integral[i] + 0.5);
-        }//计算均衡化过后对应灰度值
-        for (int i = 0; i < frOut.rows; i++)
+            EqualizeSumPix[i] = cvRound((double)nSumProDis[i] * 255);
+        }
+
+        Mat resultImage(nRows, nCols, frIn.type());
+        for (int i = 0; i < nRows; i++)
         {
-            uchar* p = frOut.ptr<uchar>(i);
-            for (int j = 0; j < frOut.cols; j++)
+            for (int j = 0; j < nCols; j++)
             {
-                int q = p[j];
-                p[j] = gray_equal[q];
+                resultImage.at<uchar>(i, j) = EqualizeSumPix[(int)frIn.at<uchar>(i, j)];
             }
         }
 
-        imshow("1",frIn);
-        imshow("2",frOut);
+
+
+         imshow("1",frame);
+         imshow("2",resultImage);
 
         Histogram1D hist;
         Mat histImg1;
-        histImg1 = hist.getHistogramImage(frIn);
+        histImg1 = hist.getHistogramImage(frame);
         Mat histImg2;
-        histImg2 = hist.getHistogramImage(frOut);
+        histImg2 = hist.getHistogramImage(resultImage);
+
+
+
         imshow("Histogram", histImg1);
         imshow("Histogram_result",histImg2);
 
-        geometry_msgs::Twist cmd_red;
 
-        // 车的速度值设置
-        cmd_red.linear.x = LINEAR_X;
-        cmd_red.linear.y = 0;
-        cmd_red.linear.z = 0;
-        cmd_red.angular.x = 0;
-        cmd_red.angular.y = 0;
-        cmd_red.angular.z = 0.2;
+		geometry_msgs::Twist cmd_red;
 
-        pub.publish(cmd_red);
+		// 车的速度值设置
+		cmd_red.linear.x = LINEAR_X;
+		cmd_red.linear.y = 0;
+		cmd_red.linear.z = 0;
+		cmd_red.angular.x = 0;
+		cmd_red.angular.y = 0;
+		cmd_red.angular.z = 0.2;
 
-        ros::spinOnce();
+		pub.publish(cmd_red);
+
+		ros::spinOnce();
 //		loop_rate.sleep();
-        waitKey(5);
+		waitKey(5);
 
-    }
+	}
 
 
-    return 0;
+	return 0;
 }
 
 
